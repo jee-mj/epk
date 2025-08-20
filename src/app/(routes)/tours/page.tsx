694b95ce180acf92315240546b76b63aecbf1353
jee-mj <28581723+jee-mj@users.noreply.github.com>
2025-08-20 12:43:28 +1000

    hotfix: htmlspecialchars

--- src/app/(routes)/tours/page.tsx
+++ src/app/(routes)/tours/page.tsx
@@ -5,7 +5,7 @@ export default function Tours() {
     <div className={styles.pageContent}>
       <section>
         <h2>Tour Schedule</h2>
-        <h3>... ooh, it's staying lonely for a few milestones.</h3>
+        <h3>... ooh, it&apos;s staying lonely for a few milestones.</h3>
       </section>
     </div>
   );
